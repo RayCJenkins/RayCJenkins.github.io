5b5179e73fd35ac362a4ce6d29d7267fa5b4479f
Ray Jenkins <reofax@gmail.com>
2016-10-09 16:51:35 -0600

    syntax error

--- views/js/main.js
+++ views/js/main.js
@@ -518,7 +518,7 @@ function updatePositions() {
     //var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
     //items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
     //items[i].style.left = items[i].basicLeft + phases[i % 5] + 'px';
-    items[i].style.translateX(phases[i % 5] + 'px')  + 'px';
+    items[i].style.transform.translateX(phases[i % 5] + 'px')  + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
