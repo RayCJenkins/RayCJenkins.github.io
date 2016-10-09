48bc3134c295ed102d621055cf6a4cdae6b8fa5d
Ray Jenkins <reofax@gmail.com>
2016-10-09 16:54:10 -0600

    whoops, went back to far

--- views/js/main.js
+++ views/js/main.js
@@ -516,7 +516,8 @@ function updatePositions() {
 
   for (var i = 0; i < items.length; i++) {
     //var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
-    items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    //items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
+    items[i].style.left = items[i].basicLeft + phases[i % 5] + 'px';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
