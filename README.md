88008334c8bce550d63ffa659988dc365629ac76
Ray Jenkins <reofax@gmail.com>
2016-10-26 21:30:01 -0600

    Changing to Neighborhood map

--- README.md
+++ README.md
@@ -1,23 +1,2 @@
-## Website Performance Optimization portfolio project
-
-This is my optimized project. The optimazations will be detailed below. I have hosted this on my github.io page for your viewing pleasure. The link to that is https://RayCJenkins.github.io
-
-### Optimizations
-
-#### Part 1: Optimize PageSpeed Insights score for index.html
-
-I made the follwoing optimazations to index.html to record a google pagespeed insights where the page received a 95/100 on the mobile tab and 97/100 on the desktop tab. The following optimazations were implememted
-
-- shrank pizzeria.jpg to 100 x 75 pixels
-- compressed both pizzeria.jpg and profile.jpg by using the webp format
-- added the async tag to the google analytics, print.css links
-- Removed the call to the google Web fonts
-- inlined all of the style.css
-- moved the print.css, google analytics, and js/perfmatters.css
-
-#### Part 2: Optimize Frames per Second in pizza.html
-
-The optimizations performed for the main.js were similar to each other. In each case it involved anaylzing a loop and reducing the time overall by moving some of the calculations outside of the loop.
-
-- In updatePositions, I pre-calculate each of the 5 phases and store them in an array, and then in the loop I just decide which index in the array to use to adjust each items left style.
-- In changePizzaSizes, I calculated the new pizza width using the 0th element of the items array and then set the width property of each item.
+# NeighborhoodMap
+This is my neighborhood map project for my fend nanodegree
---
+++ js/script.js
@@ -0,0 +1,160 @@
+var map;
+var infoWindow;
+var markers = [];
+var selectedMarker;
+var LocationCategories = ['ALL', 'Parks', 'Schools', 'Stores', 'Restaurants', 'Golf Courses']
+var interestingLocations = [
+	{
+		title: 'Sugar House Park',
+		category: 'Parks',
+		location: {lat: 40.725594, lng: -111.850862}
+	},
+	{
+		title: 'Olive Garden',
+		category: 'Restaurants',
+		location: {lat: 40.721535, lng: -111.854360}
+	},
+	{
+		title: 'Westminster College',
+		category: 'Schools',
+		location: {lat: 40.731974, lng: -111.854746}
+	},
+	{
+		title: 'Highland High School',
+		category: 'Schools',
+		location: {lat: 40.723519, lng: -111.843545}
+	},
+	{
+		title: 'Nibley Park Golf Course',
+		category: 'Golf Courses',
+		location: {lat: 40.709938, lng: -111.874015}
+	},
+	{
+		title: 'Red Lobster',
+		category: 'Restaurants',
+		location: {lat: 40.720543, lng: -111.853931}
+	},
+	{
+		title: 'Patagonia Outlet',
+		category: 'Stores',
+		location: {lat: 40.720917, lng: -111.858308}
+	},
+	{
+		title: 'Forest Dale Golf Course',
+		category: 'Golf Courses',
+		location: {lat: 40.718770, lng: -111.865046}
+	}
+]
+
+var Location = function(data, index) {
+	this.title = ko.observable(data.title);
+	this.category = ko.observable(data.category);
+	this.location = ko.observable(data.location);
+	this.isVisible = ko.observable(true);
+	this.markerIndex = ko.observable(index);
+}
+
+var ViewModel = function() {
+	var self = this;
+
+	this.Locations = ko.observableArray([]);
+
+	interestingLocations.forEach(function(locationItem, index) {
+		self.Locations.push(new Location(locationItem, index));
+	});
+	this.availableCategories = ko.observableArray(LocationCategories)
+	this.currentFilter = ko.observable('All');
+	this.currentFilter.subscribe(function(newValue) {
+		closeInfoWindowAndClearBounce(infoWindow);
+		var this_location;
+		for(var i = 0; i < this.Locations().length; i++)
+		{
+			this_location = this.Locations()[i];
+			this_location.isVisible((this_location.category() == newValue) || (newValue == 'ALL'));
+			if (markers[i])	{
+				markers[i].setVisible(this_location.isVisible());
+			}
+		};
+    }, this);
+    this.clickMapMarker = function(clickedLocation) {
+    	fillInfoWindowAndToggleMarker(markers[clickedLocation.markerIndex()]);
+    	// toggleBounce(markers[clickedLocation.markerIndex()]);
+    }
+}
+
+ko.applyBindings(new ViewModel());
+
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+        	center: {lat: 40.725864, lng: -111.850874},
+        	zoom: 15,
+        	mapTypeControl: false
+        });
+
+	infoWindow = new google.maps.InfoWindow();
+	var mapBounds = new google.maps.LatLngBounds();
+
+	for (var i = 0; i < interestingLocations.length; i++) {
+      var marker = new google.maps.Marker({
+          map: map,
+          position: interestingLocations[i].location,
+          title: interestingLocations[i].title,
+          animation: google.maps.Animation.DROP,
+          id: i
+        });
+      markers.push(marker);
+      marker.addListener('click', function() {
+      	fillInfoWindowAndToggleMarker(this);
+      });
+      mapBounds.extend(marker.position);
+	}
+	map.fitBounds(mapBounds);
+}
+
+function fillInfoWindowAndToggleMarker(marker) {
+	if (selectedMarker) {
+    	toggleBounce(selectedMarker);
+    }
+    fillInfoWindow(marker, infoWindow);
+    toggleBounce(marker);
+    selectedMarker = marker;
+}
+
+function toggleBounce(marker) {
+	if (marker.getAnimation() !== null) {
+  		marker.setAnimation(null);
+	} else {
+  		marker.setAnimation(google.maps.Animation.BOUNCE);
+	}
+}
+
+
+function fillInfoWindow(marker, infowindow) {
+	if (infowindow.marker != marker) {
+          infowindow.marker = marker;
+          infowindow.setContent('<div class="info-window">' + marker.title + '</div>');
+          infowindow.open(map, marker);
+          infowindow.addListener('closeclick', function() {
+            infowindow.marker = null;
+          });
+	}
+}
+
+function closeInfoWindowAndClearBounce(infowindow) {
+	if (infowindow){
+		infowindow.close();
+		if (infowindow.marker){
+			infowindow.marker.setAnimation(null);
+			infowindow.marker = null;
+		}
+	}
+	selectedMarker = null;
+}
+
+
+
+
+
+
+
+
